badf624d1771396c6ec79fdb900cb379b473904a
Botao <wxt2005@gmail.com>
2017-12-16 13:39:10 +0800

    Change item schema

--- script/schema/item.js
+++ script/schema/item.js
@@ -2,15 +2,17 @@ const Joi = require('joi');
 
 const validLang = ['ja', 'en', 'zh-Hans', 'zh-Hant'];
 const titleTranslateKeySchema = {};
-
 validLang.forEach((lang) => {
     titleTranslateKeySchema[lang] = Joi.array().items(Joi.string().trim().required());
 });
 
+const validType = ['tv', 'web', 'movie', 'ova'];
+
 module.exports = Joi.object().keys({
     title: Joi.string().trim().required(),
     titleTranslate: Joi.object().keys(titleTranslateKeySchema).required(),
-    lang: Joi.string().valid(validLang),
+    type: Joi.string().valid(validType).required(),
+    lang: Joi.string().valid(validLang).required(),
     officialSite: Joi.string().uri().required().allow(''),
     begin: Joi.string().isoDate().required().allow(''),
     end: Joi.string().isoDate().required().allow(''),
@@ -18,14 +20,13 @@ module.exports = Joi.object().keys({
     sites: Joi.array().items(Joi.object().keys({
         site: Joi.string().trim().required(),
         id: Joi.string().trim(),
-        keyword: Joi.string().trim(),
         url: Joi.string().regex(/^https?:\/\/.+$/).trim(),
         /** onair only begins */
         begin: Joi.string().isoDate().allow(''),
-        official: Joi.boolean(),
-        premuiumOnly: Joi.boolean(),
-        censored: Joi.boolean(),
-        lang: Joi.string().valid(validLang),
+        official: Joi.boolean().allow(null),
+        premuiumOnly: Joi.boolean().allow(null),
+        censored: Joi.boolean().allow(null),
+        exist: Joi.boolean().allow(null),
         comment: Joi.string().trim().allow('')
         /** onair only ends */
     }))
